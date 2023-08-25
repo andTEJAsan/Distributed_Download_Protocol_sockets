ed421b6abbb7d81cd61349553119ebc99ad6df35
tejas <tejasanand.iitdcse@gmail.com>
2023-08-25 18:04:01 +0530

    some strategies

--- backup/A2final.py
+++ backup/A2final.py
@@ -3,9 +3,7 @@ import threading as th
 from threading import Thread
 from threading import Lock
 import time
-from time import sleep
 import sys
-import pickle
 #maybe for starting off we can output all the lines in the file that we read from the server and send those over.
 ##The basic variables we need to use for the thread that gets new data from the server sending the lines.
 
@@ -25,7 +23,7 @@ all_lines_lock = Lock();
 all_my_lines_lock = Lock(); 
 hash_lines_lock = Lock(); 
 total_lines = 1000; #for now.
-startTime = time.time(); 
+
 def send_data(socket, data):
     #the protocol to send anything to the server is simple. We first send it the number of bytes to receive and then the data, so the 
     #receiving end knows how much data to receive, and when to stop the recv loop. 
@@ -62,31 +60,17 @@ def receive_data(socket):
 
 def getLinesFromServer(s): #s is the socket.
     send_data(s, "GET\n"); #we send the get request to the server.
-    data = receive_data(s); 
-    if(data == '' or data == None):
-        return;
-    data = data.split('\n'); #we receive the data from the server.
-    if(len(data) <= 1):
-        return;
-    if(data[-1] == ''):
-        data.pop(); 
-        # if(len(data) == 0):
-        #     break;
+    data = receive_data(s).split('\n'); #we receive the data from the server.
     #now we need to check in our current list if we have this particular data and if we do not then we should add this.
     with all_lines_lock:
         with hash_lines_lock: 
             l = len(data); 
             for i in range(0,l,2):
-                try:
-                    if(hash_lines[int(data[i])] == -1):
-                        newLine = data[i] + "\n" + data[i+1] + "\n";
-                        all_lines.append(newLine); 
-                        hash_lines[int(data[i])] = 1; #we have seen this line now.
-                        #print("added line: ", int(data[i])); 
-                except Exception as e:
-                    print("error in adding line: ", data); 
-                    print("EXACT: ", data[i]);
-                    raise e;
+                if(hash_lines[int(data[i])] == -1):
+                    newLine = data[i] + "\n" + data[i+1] + "\n";
+                    all_lines.append(newLine); 
+                    hash_lines[int(data[i])] = 1; #we have seen this line now.
+
 
 def clientThread(csocket, addr):
     lastCall = 0; #the last call that the client made to the server.
@@ -103,23 +87,21 @@ def clientThread(csocket, addr):
                 end_index = len(all_lines); 
                 for i in range(from_index, end_index): 
                     data.append(all_lines[i]);
-                data = ''.join(data); 
+                data = ''.join(data);
                 lastCall = end_index; 
                 send_data(csocket, data); #we send the data to the client.
     csocket.close(); 
 
-def mainThread(mainSocket):
+def mainThread():
     errors = 0; 
+    mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mainSocket.connect(("vayu.iitd.ac.in", 9801));
     sendline = "SENDLINE\n".encode(); 
     while(True):
         mainSocket.send(sendline); #gets a line from the server.
         try:
-            line = "";
-            while(True):
-                data = mainSocket.recv(100000).decode(); 
-                line += data; 
-                if(data[-1] == '\n'):
-                    break; 
+            line = mainSocket.recv(10000).decode(); 
+            # line = receive_data(mainSocket); 
             lineNumber = int(line.split('\n')[0]); #to get the line number.
         except Exception as e:
             #print(line + ":: raised the error " + str(e));
@@ -127,7 +109,7 @@ def mainThread(mainSocket):
             errors += 1;
             continue;
         if(lineNumber == -1):
-            getLinesFromServer(server1); 
+#            getLinesFromServer(server1); 
             continue; #not a useful result for us, we can just continue;.
         # with all_lines_lock:
         #     with hash_lines_lock:
@@ -136,48 +118,12 @@ def mainThread(mainSocket):
             hash_lines[lineNumber] = line; #so we update this line in our hashset, while also adding this to our list of lines.
             all_lines.append(line); 
             all_my_lines.append(line); 
-            if(len(all_lines) % 100) == 0:
-                print(len(all_lines), " total lines done", "added line: ", lineNumber); 
+            print(len(all_lines), " total lines done", "added line: ", lineNumber); 
         if(len(all_lines) >= total_lines):
             break; #since we have reached the end and all the lines have been read.
     print("total errors: ", errors);
-    endTime = time.time(); 
-    # server1.detach(); 
-    print("time taken for " + str(len(all_lines)) + " lines: " + str(endTime - startTime));  
-    check_code(all_lines, mainSocket); 
 
 
-def submitCode(all_lines, mainSocket):
-    result = "SUBMIT\n2021CS10556@QuickFetchers\n" + str(len(all_lines)) + "\n" + ''.join(all_lines);
-    mainSocket.send(result.encode()); 
-    result = mainSocket.recv(100000).decode(); 
-    print(result); 
-
-def check_code(all_lines, mainSocket):
-    with open("hash_lines.pickle", "rb") as f:
-        hash_lines = pickle.load(f);
-    arr = all_lines; 
-    hash = [0]*1001;
-    for i in range(0, len(arr), 1):
-        line_number = int(arr[i].split('\n')[0]); 
-        try:
-            hash[line_number] = arr[i]; 
-        except Exception as e:
-            print(f"error in adding line{arr[i]}"); 
-            print("line_number: ", line_number);
-            raise e;
-    with open("double_hash.pickle", "wb") as f:
-        pickle.dump(hash, f);
-    if(hash_lines == hash):
-        print("the lines are the same");
-    else:
-        print("lines differ"); 
-    diff = []; 
-    for i in range(0, len(hash_lines), 1):
-        if(hash_lines[i] != hash[i]):
-            diff.append(i);
-    print("diff: ", diff); 
-    submitCode(all_lines, mainSocket); 
 
 
 def myServer(port = 12345):
@@ -195,12 +141,10 @@ def myServer(port = 12345):
         Thread(target = clientThread, args = (client, addr)).start(); 
         #time.sleep(0.1); #so that the client can connect to the server.
 # Thread(target = mainThread()).start(); 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     Thread(target = myServer(), args = ()).start(); 
-    mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    mainSocket.connect(("vayu.iitd.ac.in", 9801)); 
     if(len(sys.argv) > 1):
         server1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM); 
-        server1.connect((sys.argv[1], 12345)); #connects to the server running at this ip.
-    Thread(target = mainThread, args = (mainSocket,)).start(); 
-    
+        server1.connect(sys.argv[1], 12345); #connects to the server running at this ip.
+    Thread(target = mainThread(), args = ()).start(); 
+
--- singlePersonA2.py
+++ singlePersonA2.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import sys
+import pickle
 
 mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mainSocket.connect(("vayu.iitd.ac.in", 9801));
@@ -14,36 +15,44 @@ if(len(sys.argv) > 1):
 errors = 0;
 maxLimit = 10000; 
 sumCounts = 0; 
-for i in range(totalTries):
-    startTime = time.time(); 
-    done = set(); 
-    allLines = [];
-    cnt = 0;
-    while(len(done) < linesToRead and cnt <= maxLimit):
-        mainSocket.send(sendline); 
-        line = mainSocket.recv(100000).decode();
-        try:
-            number = int(line.split('\n')[0]);
-        except Exception as e:
-            print(line + ":: raised the error " + str(e));
-            raise e; 
-            errors+=1; 
-            continue; 
-        if(number == -1):
-            continue; 
-        if(number not in done):
-            done.add(number);
-            allLines.append(line);
-        cnt += 1;
-        if(cnt % 100 == 0 and totalTries == 1):
-            print("{0} calls done, {1} lines found, {2} curLine".format(cnt, len(done), number));
-    endTime = time.time();
-    print("time taken for " + str(len(done)) + " lines: " + str(endTime - startTime) + " with total errors: " + str(errors)); 
-    result = "SUBMIT\n2021CS10556@QuickFetchers\n" + str(linesToRead) + "\n" + ''.join(allLines);
-    mainSocket.send(result.encode());
-    result = mainSocket.recv(100000).decode();
-    print(result); 
-    sumCounts += cnt;
-    print(cnt, " calls, average so far: ", sumCounts/(i+1)); 
+startTime = time.time(); 
+done = set(); 
+allLines = [];
+cnt = 0;
+while(len(done) < linesToRead and cnt <= maxLimit):
+    mainSocket.send(sendline); 
+    line = mainSocket.recv(100000).decode();
+    try:
+        number = int(line.split('\n')[0]);
+    except Exception as e:
+        #print(line + ":: raised the error " + str(e));
+        # raise e; 
+        errors+=1; 
+        continue; 
+    if(number == -1):
+        continue; 
+    if(number not in done):
+        done.add(number);
+        allLines.append(line);
+    cnt += 1;
+    if(cnt % 100 == 0 and totalTries == 1):
+        print("{0} calls done, {1} lines found, {2} curLine".format(cnt, len(done), number));
+endTime = time.time();
+print("time taken for " + str(len(done)) + " lines: " + str(endTime - startTime) + " with total errors: " + str(errors)); 
+result = "SUBMIT\n2021CS10556@QuickFetchers\n" + str(linesToRead) + "\n" + ''.join(allLines);
+mainSocket.send(result.encode()); 
+result = mainSocket.recv(100000).decode(); 
 
+hash_lines = [0]*1001; 
+for line in allLines:
+    number = int(line.split('\n')[0]);
+    hash_lines[number] = line;
+
+# with open("hash_lines.pickle", "wb") as f:
+#     pickle.dump(hash_lines, f);
+
+
+print(result); 
+sumCounts += cnt;
+print(cnt, " calls, average so far: ", sumCounts/(1)); 
 mainSocket.close(); 
